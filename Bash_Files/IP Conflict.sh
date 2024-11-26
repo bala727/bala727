326d485df530249a0c6d5179e9723cb854b87272
bala727 <balakrishnan727@gmail.com>
2024-11-26 17:24:11 +0530

    IP Conflict - bash script commit-4

---
+++ Bash_Files/IP Conflict.sh
@@ -0,0 +1,9 @@
+ #!/bin/bash 
+# Script to check ipconflict
+# Grep the word "Dup" and enable a pop up to show that there is ip conflict
+
+sudo arp-scan -I wlp1s0 -l | grep DUP
+
+if [[ $? == 0 ]]; then
+  notify-send "Ip-conflict detected"
+fi
